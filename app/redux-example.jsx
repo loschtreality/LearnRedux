45953a4d3d0f64032ceafc8f16268496dc303b43
Michael Loschiavo <losch103@gmail.com>
2016-07-22 14:25:31 -0700

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,14 +2,55 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: "Anonymous"},action) => {
-  // state = state || {name: "Anonymous"}
+var stateDefault = {
+  name: "Anonymous",
+  hobbies: [],
+  movies: []
+}
+var nextHobbyID = 1
+var nextMovieID = 1
+var reducer = (state = stateDefault,action) => {
+
   switch (action.type) {
     case 'CHANGE_NAME':
     return {
       ...state,
       name: action.name
     }
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyID++,
+            hobby: action.hobby
+          }
+        ]
+      }
+    case 'ADD_MOVIE':
+      return{
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieID++,
+            movie: action.movie,
+            genre: action.genre
+          }
+        ]
+      }
+      case 'REMOVE_HOBBY':
+        return {
+          ...state,
+          hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+        }
+      case 'REMOVE_MOVIE':
+        return {
+          ...state,
+          movies: state.movies.filter((film) => film.id !== action.id)
+        }
+
     default:
     return state
   }
@@ -24,21 +65,57 @@ var unsubscribe = store.subscribe(
   () => {
   var state = store.getState()
 
-  console.log('Name is', state.name);
+  // console.log('Name is', state.name);
   document.getElementById('app').innerHTML = state.name
+
+
+  console.log('new state ', store.getState());
   }
 )
 // unsubscribe()
 
 var currentState = store.getState()
 
-console.log('currentState', currentState);
+// console.log('currentState', currentState);
 
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Michael'
 })
 
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+})
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+})
+
+store.dispatch({
+  type: "REMOVE_HOBBY",
+  id: 2
+})
+
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: 'Fight Club',
+  genre: 'Fiction',
+})
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: 'Batman',
+  genre: 'Fiction',
+})
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
+})
 
 store.dispatch({
   type: 'CHANGE_NAME',
